eea85fea170b0453fa96d724172967fe9986f34d
Petr Machata <petrm@nvidia.com>
2022-03-25 18:29:17 +0300

    ipstats: Add a "set" command
    Add a command to allow toggling HW stats. An example usage:
    
     # ip stats set dev swp1 l3_stats on
    
    Signed-off-by: Petr Machata <petrm@nvidia.com>

--- ip/ipstats.c
+++ ip/ipstats.c
@@ -1,58 +1,90 @@
 // SPDX-License-Identifier: GPL-2.0+
+#include <errno.h>
+
 #include "utils.h"
 #include "ip_common.h"
 
-static const struct ipstats_stat_desc *ipstats_stat_desc_toplev_subs[] = {
-};
-
-static const struct ipstats_stat_desc ipstats_stat_desc_toplev_group = {
-	.name = "top-level",
-	.kind = IPSTATS_STAT_DESC_KIND_GROUP,
-	.subs = ipstats_stat_desc_toplev_subs,
-	.nsubs = ARRAY_SIZE(ipstats_stat_desc_toplev_subs),
-};
-
 static int do_help(void)
 {
-	const struct ipstats_stat_desc *toplev = &ipstats_stat_desc_toplev_group;
-	int i;
-
 	fprintf(stderr,
 		"Usage: ip stats help\n"
+		"       ip stats set dev DEV l3_stats { on | off }\n"
 		);
 
-	for (i = 0; i < toplev->nsubs; i++) {
-		const struct ipstats_stat_desc *desc = toplev->subs[i];
-
-		if (i == 0)
-			fprintf(stderr, "G := { %s", desc->name);
-		else
-			fprintf(stderr, " | %s", desc->name);
-	}
-	if (i > 0)
-		fprintf(stderr, " }\n");
-
-	for (i = 0; i < toplev->nsubs; i++) {
-		const struct ipstats_stat_desc *desc = toplev->subs[i];
-		size_t j;
-
-		if (desc->kind != IPSTATS_STAT_DESC_KIND_GROUP)
-			continue;
-
-		for (j = 0; j < desc->nsubs; j++) {
-			const struct ipstats_stat_desc *sub = desc->subs[j];
-
-			if (j == 0)
-				fprintf(stderr, "%s SG := { %s",
-					desc->name, sub->name);
-			else
-				fprintf(stderr, " | %s", sub->name);
-		}
-		if (j > 0)
-			fprintf(stderr, " }\n");
-	}
-
 	return 0;
+}
+
+static int ipstats_set_do(int ifindex, int at, bool enable)
+{
+	struct ipstats_req req = {
+		.nlh.nlmsg_len = NLMSG_LENGTH(sizeof(struct if_stats_msg)),
+		.nlh.nlmsg_flags = NLM_F_REQUEST,
+		.nlh.nlmsg_type = RTM_SETSTATS,
+		.ifsm.family = PF_UNSPEC,
+		.ifsm.ifindex = ifindex,
+	};
+
+	addattr8(&req.nlh, sizeof(req), at, enable);
+
+	if (rtnl_talk(&rth, &req.nlh, NULL) < 0)
+		return -2;
+	return 0;
+}
+
+static int ipstats_set(int argc, char **argv)
+{
+	const char *dev = NULL;
+	bool enable = false;
+	int ifindex;
+	int at = 0;
+
+	while (argc > 0) {
+		if (strcmp(*argv, "dev") == 0) {
+			NEXT_ARG();
+			if (dev)
+				duparg2("dev", *argv);
+			if (check_ifname(*argv))
+				invarg("\"dev\" not a valid ifname", *argv);
+			dev = *argv;
+		} else if (strcmp(*argv, "l3_stats") == 0) {
+			int err;
+
+			NEXT_ARG();
+			if (at) {
+				fprintf(stderr, "A statistics suite to toggle was already given.\n");
+				return -EINVAL;
+			}
+			at = IFLA_STATS_SET_OFFLOAD_XSTATS_L3_STATS;
+			enable = parse_on_off("l3_stats", *argv, &err);
+			if (err)
+				return err;
+		} else if (strcmp(*argv, "help") == 0) {
+			do_help();
+			return 0;
+		} else {
+			fprintf(stderr, "What is \"%s\"?\n", *argv);
+			do_help();
+			return -EINVAL;
+		}
+
+		NEXT_ARG_FWD();
+	}
+
+	if (!dev) {
+		fprintf(stderr, "Not enough information: \"dev\" argument is required.\n");
+		exit(-1);
+	}
+
+	if (!at) {
+		fprintf(stderr, "Not enough information: stat type to toggle is required.\n");
+		exit(-1);
+	}
+
+	ifindex = ll_name_to_index(dev);
+	if (!ifindex)
+		return nodev(dev);
+
+	return ipstats_set_do(ifindex, at, enable);
 }
 
 int do_ipstats(int argc, char **argv)
@@ -65,6 +97,8 @@ int do_ipstats(int argc, char **argv)
 	} else if (strcmp(*argv, "help") == 0) {
 		do_help();
 		rc = 0;
+	} else if (strcmp(*argv, "set") == 0) {
+		rc = ipstats_set(argc-1, argv+1);
 	} else {
 		fprintf(stderr, "Command \"%s\" is unknown, try \"ip stats help\".\n",
 			*argv);
